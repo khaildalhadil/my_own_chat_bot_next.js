186c4f5ac13c00dd18b6e934974ac135656c5641
khaildalhadil <khaild.alhadi2020@gamil.com>
2025-09-17 16:25:20 +0400

    change the color of the first page

--- components/FirstPage.jsx
+++ components/FirstPage.jsx
@@ -3,7 +3,7 @@ import { RiRobot3Line } from 'react-icons/ri'
 
 export default function FirstPage() {
   return (
-    <div className='h-full text-white text-2xl flex justify-center items-center flex-col'>
+    <div className='h-full text-[#444] text-2xl flex justify-center items-center flex-col'>
       <RiRobot3Line className='text-7xl' />
       <p className='rounded px-3 pb-2 text-center'>اسالني عن طريق الكتابه تحت <br />Ask me a question !</p>
     </div>
